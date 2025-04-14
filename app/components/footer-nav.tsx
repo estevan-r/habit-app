12e36992b637088afc68978f53bbb8db6c3181fc
estevan-r <ruiz.estevan@gmail.com>
2025-04-13 20:39:35 -0600

    Add active link styles

--- app/components/footer-nav.tsx
+++ app/components/footer-nav.tsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router'
 import { Button } from '~/components/ui/button'
 import { Avatar, AvatarImage, AvatarFallback } from '~/components/ui/avatar'
-//TODO: Add styles for active links
+
 export default function FooterNav() {
   return (
     <div className='fixed z-20 bottom-0 flex justify-center p-4 w-full'>
       <div className='flex justify-center items-center gap-6 bg-stone-100/30 p-3 rounded-full outline-2 outline-stone-950/5 backdrop-blur-sm shadow-xl'>
-        <NavLink to='/'>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            isActive ? 'ring-2 ring-emerald-300/80 rounded-full' : ''
+          }
+        >
           <Button
             variant='outline'
             size='icon'
@@ -15,12 +20,22 @@ export default function FooterNav() {
             ⌂
           </Button>
         </NavLink>
-        <NavLink to='/create'>
+        <NavLink
+          to='/create'
+          className={({ isActive }) =>
+            isActive ? 'ring-2 ring-emerald-300/80 rounded-full' : ''
+          }
+        >
           <Button size='icon' className='size-8 shrink-0 rounded-full'>
             +
           </Button>
         </NavLink>
-        <NavLink to='/profile'>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            isActive ? 'ring-2 ring-emerald-300/80 rounded-full' : ''
+          }
+        >
           <Avatar>
             <AvatarImage src='https://github.com/shadcn.png' />
             <AvatarFallback>ER</AvatarFallback>
