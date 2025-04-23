1878332fcf5500b40edf350d0f2191ee86359baf
estevan-r <ruiz.estevan@gmail.com>
2025-04-23 00:54:41 -0600

    Resolve reset button

--- app/components/reset-habits-button.tsx
+++ app/components/reset-habits-button.tsx
@@ -3,18 +3,15 @@ import { useHabits } from '~/context/HabitsContext'
 // import { resetHabits } from '~/store/db'
 
 export default function ResetHabitsButton() {
-  const handleClick = () => {
-    useHabits().reset()
-    window.location.reload()
-  }
+  const { reset } = useHabits()
+
+  // const handleClick = () => {
+  //   useHabits().reset()
+  //   window.location.reload()
+  // }
 
   return (
-    <Button
-      variant='destructive'
-      size='lg'
-      onClick={handleClick}
-      className='w-full'
-    >
+    <Button variant='destructive' size='lg' onClick={reset} className='w-full'>
       Reset All Habits
     </Button>
   )
