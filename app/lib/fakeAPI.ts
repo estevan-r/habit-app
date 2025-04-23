ebe19ecc3621b0e5bb2eed7f812973766ddb712d
estevan-r <ruiz.estevan@gmail.com>
2025-04-23 00:06:25 -0600

    Create types and fake api

---
+++ app/lib/fakeAPI.ts
@@ -0,0 +1,40 @@
+import habitsData from '~/data/habits.json'
+import { type Habit } from '~/types'
+
+let habits = [...habitsData]
+
+export function getHabits(): Promise<Habit[]> {
+  return new Promise((res) => setTimeout(() => res([...habits]), 300))
+}
+
+export function getHabi(id: string): Promise<Habit | undefined> {
+  return new Promise((res) =>
+    setTimeout(() => res(habits.find((h) => h.id === id)), 300)
+  )
+}
+
+export function markHabitAsDone(id: string): Promise<Habit | null> {
+  return new Promise((res) => {
+    setTimeout(() => {
+      const index = habits.findIndex((h) => h.id === id)
+      if (index === -1) return res(null)
+
+      const today = new Date().toISOString()
+      const updated = {
+        ...habits[index],
+        currentStreak: habits[index].currentStreak + 1,
+        lastCompletedDate: today,
+      }
+
+      habits[index] = updated
+      res(updated)
+    }, 300)
+  })
+}
+
+export function resetHabits(): Promise<void> {
+  return new Promise((res) => {
+    habits = [...habitsData]
+    setTimeout(res, 300)
+  })
+}
---
+++ app/types.ts
@@ -0,0 +1,8 @@
+export type Habit = {
+  id: string
+  name: string
+  description: string
+  interval: string
+  currentStreak: number
+  lastCompletedDate: string
+}
