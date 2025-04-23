6595e752dcd4ca685a3317a7ddf9c82924e67793
estevan-r <ruiz.estevan@gmail.com>
2025-04-23 00:45:45 -0600

    Update mark as done button

--- app/components/mark-as-done-button.tsx
+++ app/components/mark-as-done-button.tsx
@@ -1,29 +1,21 @@
 import { Button } from '~/components/ui/button'
-import { type Habit, markHabitAsDone } from '~/store/db'
+// import { type Habit, markHabitAsDone } from '~/store/db'
 import { isToday, parseISO } from 'date-fns'
 
-type Props = {
-  habit: Habit
-  onUpdate: (updatedHabit: Habit) => void
-}
+// type Props = {
+//   habit: Habit
+//   onUpdate: (updatedHabit: Habit) => void
+// }
 
-export default function MarkAsDoneButton({ habit, onUpdate }: Props) {
-  const alreadyDoneToday = isToday(parseISO(habit.lastCompletedDate))
+export default function MarkAsDoneButton({ onDone }: { onDone: () => void }) {
+  // const alreadyDoneToday = isToday(parseISO(habit.lastCompletedDate))
 
-  const handleClick = () => {
-    const updated = markHabitAsDone(habit.id)
-    if (updated) {
-      onUpdate(updated)
-    }
-  }
+  // const handleClick = () => {
+  //   const updated = markHabitAsDone(habit.id)
+  //   if (updated) {
+  //     onUpdate(updated)
+  //   }
+  // }
 
-  return (
-    <Button
-      onClick={handleClick}
-      disabled={alreadyDoneToday}
-      variant={alreadyDoneToday ? 'secondary' : 'default'}
-    >
-      {alreadyDoneToday ? 'Done Today ✅' : 'Mark as Done'}
-    </Button>
-  )
+  return <Button onClick={onDone}>Mark as Done</Button>
 }
